20d244536f0d60b7de2d39a9954dd510beaf245f
Nfuu <nicolas@fulghumgap.de>
2022-02-02 16:57:27 +0000

    updated db with additional ranks

--- db_tables/scripts/api_generation_yearly.sql
+++ db_tables/scripts/api_generation_yearly.sql
@@ -1,6 +1,6 @@
 INSERT IGNORE INTO published.api_generation_yearly (country_or_region, country_code, "year", variable,
-    generation_twh, share_of_generation_pct, capacity_gw, emissions_mtco2, continent, eu_member_flag, 
-    g20_flag, oecd_flag, region_demand_rank, latest_year, coal_deadline, clean_deadline)
+    generation_twh, share_of_generation_pct, capacity_gw, emissions_mtco2, continent, ember_region, eu_member_flag, 
+    g20_flag, oecd_flag, region_demand_rank, global_fuel_rank, latest_year, coal_deadline, clean_deadline)
 WITH latest_year as(
     SELECT country_code, MAX("year") as max_year 
     FROM published.mart_generation_yearly_global 
@@ -12,14 +12,14 @@ WITH latest_year as(
         row_number() OVER(PARTITION BY continent ORDER BY demand_twh DESC) as region_demand_rank
     FROM mart_country_overview_yearly_global
     WHERE "year" = {api_year} - 1
-), deadlines as(
+), global_fuel_rank as(
     SELECT
         country_name,
-        coal_deadline,
-        clean_deadline
-    FROM dim_country
-)
-SELECT
+        row_number() OVER(PARTITION BY global_fuel_desc ORDER BY generation_twh DESC) as global_fuel_rank
+    FROM mart_generation_yearly_global
+    WHERE "year" = {api_year} - 1
+), expanded_table as(
+	SELECT
     generation.country_name as country_or_region,
     generation.country_code,
     "year",
@@ -27,20 +27,48 @@ SELECT
     generation_twh,
     share_of_generation_pct,
     capacity_gw,
-    emissions_estimate_mtco2 as emissions_mtco2,
-    continent,
-    eu_member_flag,
-    g20_flag,
-    oecd_flag,
+    emissions_estimate_mtco2 as emissions_mtco2
+    FROM published.mart_generation_yearly_global generation
+    UNION
+	SELECT
+    country_name,
+    country_code,
+    "year",
+    CASE WHEN fossil_flag = 1 THEN 'Fossil' ELSE 'Clean' END as variable,
+    SUM(generation_twh) as generation_twh,
+    SUM(share_of_generation_pct) as share_of_generation_pct,
+    SUM(capacity_gw) as capacity_gw,
+    SUM(emissions_estimate_mtco2) as emissions_estimate_mtco2
+    FROM mart_generation_yearly_global
+    GROUP BY country_name, country_code, "year", variable
+)
+SELECT
+    generation.country_or_region as country_or_region,
+    generation.country_code,
+    generation."year",
+    generation.variable as variable,
+    generation.generation_twh as generation_twh,
+    generation.share_of_generation_pct as share_of_generation_pct,
+    generation.capacity_gw as capacity_gw,
+    generation.emissions_mtco2 as emissions_mtco2,
+    country.continent as continent,
+    country.ember_region as ember_region,
+    country.eu_member_flag as eu_member_flag,
+    country.g20_flag as g20_flag,
+    country.oecd_flag as oecd_flag,
     region_demand_rank.region_demand_rank as region_demand_rank,
+    global_fuel_rank.global_fuel_rank as global_fuel_rank,
     latest_year.max_year as latest_year,
-    deadlines.coal_deadline as coal_deadline,
-    deadlines.clean_deadline as clean_deadline
-FROM published.mart_generation_yearly_global generation
+    country.coal_deadline as coal_deadline,
+    country.clean_deadline as clean_deadline
+FROM expanded_table generation
 LEFT JOIN latest_year
     ON generation.country_code = latest_year.country_code
 LEFT JOIN region_demand_rank
-    ON generation.country_name = region_demand_rank.country_name
-LEFT JOIN deadlines
-    ON generation.country_name = deadlines.country_name
+    ON generation.country_or_region = region_demand_rank.country_name
+LEFT JOIN global_fuel_rank
+    ON generation.country_or_region = global_fuel_rank.country_name
+LEFT JOIN dim_country country
+    ON generation.country_or_region = country.country_name
 WHERE "year" <= {api_year}
+ORDER BY country_or_region, "year", variable
