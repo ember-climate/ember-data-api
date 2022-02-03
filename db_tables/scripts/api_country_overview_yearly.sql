9d3f1a5705d2d6043aaf14e0cf122097e0df7325
Nfuu <nicolas@fulghumgap.de>
2022-02-03 14:56:35 +0000

    added deadlines for regions

--- db_tables/scripts/api_country_overview_yearly.sql
+++ db_tables/scripts/api_country_overview_yearly.sql
@@ -1,45 +1,73 @@
-INSERT IGNORE INTO published.api_country_overview_yearly (country_or_region, country_code, "year", 
+INSERT INTO published.api_country_overview_yearly (country_or_region, country_code, "year", 
     demand_twh, demand_mwh_per_capita, emissions_intensity_gco2_per_kwh, continent, ember_region, eu_member_flag, g20_flag, 
-    oecd_flag, region_demand_rank, latest_year, coal_deadline, clean_deadline)
-WITH latest_year as(
-    SELECT country_code, MAX("year") as max_year 
-    FROM published.mart_country_overview_yearly_global 
-    WHERE "year" <= {api_year}
-    GROUP BY country_code
-), region_demand_rank as(
+    g7_flag, oecd_flag, region_demand_rank, latest_year, coal_deadline, clean_deadline)
+WITH region_demand_rank as(
     SELECT
         country_name,
-        row_number() OVER(PARTITION BY continent ORDER BY demand_twh DESC) as region_demand_rank
+        row_number() OVER(PARTITION BY ember_region ORDER BY demand_twh DESC) as region_demand_rank
     FROM mart_country_overview_yearly_global
     WHERE "year" = {api_year} - 1
 ), deadlines as(
     SELECT
-        country_name,
+        country_name as country_or_region,
         coal_deadline,
         clean_deadline
     FROM dim_country
+    UNION
+    SELECT
+        region as country_or_region,
+        coal_deadline,
+        clean_deadline
+    FROM dim_region
+), combined_overview as (
+    SELECT
+        country_name as country_or_region,
+        country_code,
+        "year",
+        demand_twh,
+        demand_mwh_per_capita,
+        emissions_intensity_estimate_gco2_per_kwh as emissions_intensity_gco2_per_kwh
+    FROM published.mart_country_overview_yearly_global
+    UNION
+    SELECT 
+        region as country_or_region,
+        NULL as country_code,
+        "year",
+        demand_twh,
+        demand_mwh_per_capita,
+        emissions_intensity_gco2_per_kwh as emissions_intensity_gco2_per_kwh
+    FROM published.mart_overview_yearly_region
+), latest_year as(
+    SELECT country_or_region, MAX("year") as max_year 
+    FROM combined_overview
+    WHERE "year" <= {api_year}
+    GROUP BY country_or_region
 )
 SELECT
-    overview.country_name as country_or_region,
+    overview.country_or_region as country_or_region,
     overview.country_code,
     "year",
     demand_twh,
     demand_mwh_per_capita,
-    emissions_intensity_estimate_gco2_per_kwh as emissions_intensity_gco2_per_kwh,
-    continent,
-    ember_region,
-    eu_member_flag,
-    g20_flag,
-    oecd_flag,
+    emissions_intensity_gco2_per_kwh,
+    country.continent,
+    country.ember_region,
+    country.eu_member_flag,
+    country.g20_flag,
+    country.g7_flag,
+    country.oecd_flag,
     region_demand_rank.region_demand_rank as region_demand_rank,
     latest_year.max_year as latest_year,
     deadlines.coal_deadline as coal_deadline,
     deadlines.clean_deadline as clean_deadline
-FROM published.mart_country_overview_yearly_global overview
+FROM combined_overview overview
 LEFT JOIN latest_year
-    ON overview.country_code = latest_year.country_code
+    ON overview.country_or_region = latest_year.country_or_region
 LEFT JOIN region_demand_rank
-    ON overview.country_name = region_demand_rank.country_name
+    ON overview.country_or_region = region_demand_rank.country_name
 LEFT JOIN deadlines
-    ON overview.country_name = deadlines.country_name
+    ON overview.country_or_region = deadlines.country_or_region
+LEFT JOIN published.dim_country as country
+    ON overview.country_or_region = country.country_name
 WHERE "year" BETWEEN 2000 AND {api_year}
+AND overview.country_or_region IS NOT NULL
