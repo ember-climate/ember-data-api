01fe0476b02cbbdfac0f0ea125b65e48b15ee89a
Nfuu <nicolas@fulghumgap.de>
2022-08-15 15:58:13 +0100

    added new price table

--- create_table.py
+++
@@ -1,9 +0,0 @@
-import sqlalchemy
-
-
-def create_table(db_con, table_name, table_structure):
-
-    if sqlalchemy.inspect(db_con.engine).has_table(table_name):
-        db_con.execute("DROP TABLE " + table_name)
-
-    db_con.execute("CREATE TABLE " + table_name + table_structure)
