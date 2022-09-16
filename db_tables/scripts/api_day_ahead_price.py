36fc1d05b0c1ea22a39258488d7aaadf81680b88
MattEwen <matt.ewen2@gmail.com>
2022-09-16 20:25:44 +0100

    fix popup date problem with terrible hack

--- db_tables/scripts/api_day_ahead_price.py
+++ db_tables/scripts/api_day_ahead_price.py
@@ -54,20 +54,30 @@ def _combine_prices(country_df: pd.DataFrame, maxmin_df: pd.DataFrame) -> pd.Dat
     return maxmin_df.merge(country_df, how='left', on='Date')
 
 
+def _add_popup_date_column(source_df: pd.DataFrame, grain: str) -> pd.DataFrame:
+    match grain.split(' ')[0]:
+        case 'daily':
+            source_df['popup_date'] = pd.to_datetime(source_df['Date']).dt.strftime('%d %b %y (%a)')
+        case 'monthly':
+            source_df['popup_date'] = pd.to_datetime(source_df['Date']).dt.strftime('%b %y')
+    return source_df
+
+
 def _get_prices(grain: str):
     prices_countries = _read_prices_countries(grain=grain)
     transformed_prices_countries = _transform_prices_country(prices_countries)
     prices_maxmin = _read_prices_maxmin(grain=grain)
     combined_prices = _combine_prices(transformed_prices_countries, prices_maxmin)
     combined_prices['Grain'] = grain.capitalize()
-    return combined_prices
+    popup_prices = _add_popup_date_column(combined_prices, grain)
+    return popup_prices
 
 
 def create_api_day_ahead_price() -> pd.DataFrame:
     monthly_prices = _get_prices(grain='monthly')
-    daily_prices_year_months = _get_prices(grain='daily (3 months)')
+    daily_prices_3_months = _get_prices(grain='daily (3 months)')
     daily_prices_year = _get_prices(grain='daily (1 year)')
-    return pd.concat([monthly_prices, daily_prices_year_months, daily_prices_year])
+    return pd.concat([monthly_prices, daily_prices_3_months, daily_prices_year])
 
 
 def main() -> pd.DataFrame:
