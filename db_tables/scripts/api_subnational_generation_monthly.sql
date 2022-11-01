fb98ca8e761563ca0c99fbe4f92c7d6f5db58f27
Nfuu <nicolas@fulghumgap.de>
2022-11-01 16:58:09 +0000

    Added subnational data from new mart table

--- db_tables/scripts/api_subnational_generation_monthly.sql
+++ db_tables/scripts/api_subnational_generation_monthly.sql
@@ -4,25 +4,22 @@ INSERT INTO published.api_subnational_generation_monthly (
     "date",
     variable,
     generation_gwh,
-    capacity_gw
+    capacity_gw,
+    emissions_mtco2
 )
 SELECT 
     CASE
         WHEN country.display_name IS NOT NULL THEN country.display_name
         ELSE country.country_name END as country,
-    generation.state_name as "state",
-    generation.generation_date as "date",
+    states.state_name as "state",
+    states.generation_date as "date",
     fuel.global_fuel_desc as variable,
-    generation.generation_gwh as generation_gwh,
-    capacity.capacity_gw as capacity_gw
-FROM transformed.trn_gen_mth_ind_states_hist generation
+    states.generation_gwh as generation_gwh,
+    states.capacity_gw as capacity_gw
+    states.emissions_mtco2 as emissions_mtco2
+FROM transformed.mart_generation_monthly_states states
 LEFT JOIN published.dim_country country
     ON country.country_code = generation.country_code
-LEFT JOIN transformed.trn_cap_mth_ind_states_hist capacity
-    ON 
-        capacity.state_code = generation.state_code
-        AND capacity.capacity_date = generation.generation_date
-        AND capacity.global_fuel_code = generation.global_fuel_code
 LEFT JOIN published.dim_global_fuel_code fuel
     ON fuel.global_fuel_code = generation.global_fuel_code
 
