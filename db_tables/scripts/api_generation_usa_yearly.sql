4d0bee583d35933b4848b67491a465d07114c7f8
Nfuu <nicolas@fulghumgap.de>
2025-03-17 11:09:13 +0000

    update 2025-03-17

--- db_tables/scripts/api_generation_usa_yearly.sql
+++ db_tables/scripts/api_generation_usa_yearly.sql
@@ -82,4 +82,4 @@ SELECT
     states.emissions_ktco2 as emissions_ktco2,
     states.share_of_emissions_pct as share_of_emissions_pct
 FROM combined as states
-WHERE states.year <= 2023
+WHERE states.year <= 2024
